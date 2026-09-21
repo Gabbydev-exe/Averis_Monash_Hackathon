531f35b82e2d4fcff47225f6b10dc7e7841e9101
zlee0096 <zlee0096@student.monash.edu>
2026-09-21 16:20:30 +0800

    Connect email API to MySQL with integration tests

--- backend/src/test/java/com/shipping/api/ShippingApiApplicationTests.java
+++ backend/src/test/java/com/shipping/api/ShippingApiApplicationTests.java
@@ -1,13 +1,19 @@
 package com.shipping.api;
 
+import com.shipping.api.repository.EmailRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(properties = "spring.profiles.active=no-db")
 class ShippingApiApplicationTests {
+    @Autowired ApplicationContext context;
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+        assertThat(context.getBeansOfType(EmailRepository.class)).isEmpty();
+    }
 }
